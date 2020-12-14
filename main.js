05d021dedcfbfc1df349f66d7360bebd02bf9e68
vrittikumra <50193084+vrittikumra@users.noreply.github.com>
2020-12-14 20:01:25 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,13 +8,12 @@ menu_list_array = ["chicken tandoori pizza",
 
 function getmenu(){
     var htmldata;
-    htmldata="<ol=class='menulist'>"
+    htmldata="<ol=class='menulist'>";
     menu_list_array.sort();
-    for (var i = 0;   menu_list_array_length; i++) {
-        htmldata=htmldata 
-        'li' + menu_list_array[i]+'</li>'
-        htmldata=htmldata+
-        "</ol>"
+    for (var i = 0;   menu_list_array.length; i++) {
+        htmldata+=<"<li>" + menu_list_array[i]+"</li>";
+        htmldata+=
+        "</ol>";
         document.getElementById("display_menu").innerHTML=htmldata;
     }
 function add_item(){
@@ -23,12 +22,11 @@ function add_item(){
     menu_list_array.push(item);
     menu_list_array.sort();
     htmldata="<section class= 'cards'>"
-    for (var i  = 0; i < menu_list_array.menu_list_array_length; i++) {
-        htmldata=htmldata+"<div class= 'card'>" + <img src="images/pizzaimg.png"/ > +menu_list_array{i} + '</div>'
+    for (var i  = 0; i < menu_list_array.menu_list_array.length; i++) {
+        htmldata=htmldata+"<div class= 'card'>" + <img src="pizzaimg.png"/ > +menu_list_array{i} + '</div>'
       
 htmldata=htmldata+"</section>"
 document.getElementById("display_addedmenu").innerHTML=htmldata;
 
     }
  
-
